154aa3bb6bfa6d6c62e1008ac03177523d32c276
Ben Fourie <ben.fourie@gmail.com>
2015-06-15 11:33:06 +0200

    Add a http request builder

---
+++ src/SocketWebClient.Tests/SocketHttpRequestBuilderTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace SocketWebClient.Tests
+{
+    public class SocketHttpRequestBuilderTests
+    {
+        [Test]
+        public void Should_build_a_request_with_the_given_method_and_url()
+        {
+            const string expected = "GET http://www.test.com/page HTTP/1.1\r\n" +
+                                    "Host: www.test.com\r\n" +
+                                    "Connection: Close\r\n";
+
+            var builder = new SocketHttpRequestBuilder(Method.GET, new Uri("http://www.test.com/page"));
+
+            string result = builder.Build();
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ src/SocketWebClient/SocketHttpRequestBuilder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace SocketWebClient
+{
+    public class SocketHttpRequestBuilder
+    {
+        public SocketHttpRequestBuilder(Method method, Uri url)
+        {
+            Method = method;
+            Url = url;
+        }
+
+        public Method Method { get; private set; }
+
+        public Uri Url { get; private set; }
+
+        public string Build()
+        {
+            var requestBuilder = new StringBuilder();
+            requestBuilder.AppendFormat("{0} {1} HTTP/1.1", Method, Url);
+            requestBuilder.AppendLine();
+            requestBuilder.AppendFormat("Host: {0}", Url.Host);
+            requestBuilder.AppendLine();
+            requestBuilder.AppendLine("Connection: Close");
+            return requestBuilder.ToString();
+        }
+    }
+}
